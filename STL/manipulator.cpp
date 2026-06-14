1c236ec0aad5efebecf23be77484a37cb66397cb
AnishJoshi12 <anishjoshi126@gmail.com>
2026-06-14 20:11:15 +0530

    Add files via upload

---
+++ STL/manipulator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    // int a=10;
+    // cout<<setfill('_');
+    // cout<<setw(8)<<a<<endl;
+    // cout<<setw(5)<<a<<setw(6)<<a<<endl;
+    // cout<<setw(3)<<a<<setw(10)<<a<<endl;
+    
+    // set precision
+    float pi=3.1452;
+    cout<<pi<<endl;
+    cout<<setprecision(3)<<pi;
+     return 0;
+}
